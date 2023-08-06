ed299e3c75f2fbb41771a5dd577880ac55c96436
David Nowinsky <david.nowinsky@toucantoco.com>
2023-08-06 17:30:34 +0200

    Duration logs for planning and execution

---
+++ src/query/performance_utils.py
@@ -0,0 +1,13 @@
+import time
+
+
+class Timer:
+    def __enter__(self):
+        self.begin = time.perf_counter()
+
+    def __exit__(self, type, value, traceback):
+        self.end = time.perf_counter()
+        self.interval = self.end - self.begin
+    
+    def __str__(self):
+        return f'{self.interval * 1000:.0f}ms'
--- src/query/query.py
+++ src/query/query.py
@@ -2,6 +2,7 @@ import logging
 import polars as pl
 from pydantic import BaseModel
 
+from .performance_utils import Timer
 from .query_source import QueryPlan, QuerySource
 from .steps import QueryStep
 
@@ -11,16 +12,21 @@ class Query(BaseModel):
     steps: list[QueryStep]
 
     def plan(self) -> QueryPlan:
-        logging.debug('Start query plan')
         pq = self.source.plan()
         for step in self.steps:
             pq = step.plan(pq)
-        logging.debug('End query plan')
         return pq
 
-
     def execute(self) -> pl.DataFrame:  # TODO maybe return pyarrow Table instead
-        logging.debug('Start query execution')
-        result = self.plan().execute()
-        logging.debug('End of query execution')
+        logging.debug(f'Start query planning')
+        timer_plan = Timer()
+        with timer_plan:
+            plan = self.plan()
+        logging.debug(f'End of query planning - duration: {timer_plan}')
+        
+        logging.debug(f'Start query execution')
+        timer_exec = Timer()
+        with timer_exec:
+            result = plan.execute()
+        logging.debug(f'End query execution - duration: {timer_exec}')
         return result
