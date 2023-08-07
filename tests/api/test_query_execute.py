92df484b0c331bb54fb0030bfff4a0ae23e7585a
David Nowinsky <david.nowinsky@toucantoco.com>
2023-08-07 07:51:32 +0200

    Basic query test through API

--- tests/api/test_query_execute.py
+++ tests/api/test_query_execute.py
@@ -1,8 +1,19 @@
 from .client import client
 
-# def test_root():
-#     response = client.post('/query/execute')
-#     assert response.status_code == 200
-#     assert response.json() == {"Hello": "World"}
-
-
+def test_simple_query(client):
+    response = client.post(
+        '/query/execute',
+        json={
+            'source': {
+                'type': 'file',
+                'file': 'tests/fixtures/sample_data.csv',
+            },
+            'steps': [],
+        }
+    )
+    assert response.status_code == 200
+    assert response.json() == [
+        {'user_id': 'A', 'value': 1},
+        {'user_id': 'B', 'value': 2},
+        {'user_id': 'C', 'value': 3},
+    ]
